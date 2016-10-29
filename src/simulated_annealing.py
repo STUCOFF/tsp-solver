7004c79a4290461b70ae271d302d6e3ddd560e77
masaponto <masaponto@gmail.com>
2016-10-29 14:32:34 +0900

    [ADD] add simulated_annealing

--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python
 
 import numpy as np
-from utils import read_graph, print_graph, swap_list_element, \
-    get_neighbors, get_cost, find_better_solusion
+import random
+
+from utils import read_graph, print_graph, get_neighbors, \
+    get_cost, find_better_solusion
 
 
-def simulated_anealing():
-    pass
+def simulated_anealing(graph, step=1000, a=0.5, q=10, t=50, t_min=0.001):
+
+    n = len(graph)
+    s = np.random.permutation(n).tolist()  # initil solution
+
+    for i in range(step):
+        s_next = find_better_solusion(get_neighbors(s), s, graph)
+
+        e = get_cost(s, graph)
+        e_next = get_cost(s_next, graph)
+
+        if e < e_next:
+            s = s_next
+        else:
+            if random.random() <= probability(e, e_next, t):
+                s = s_next
+
+        if i % q == 0:  # Equilibrium state
+            t = a * t
+
+        if t < t_min:
+            return s
+
+    return s
 
 
 def probability(e1, e2, t):
-    pass
+    return np.exp((e1 - e2) / t)
 
 
 def main():
-    pass
+    graph = read_graph("graph.txt")
+    s = simulated_anealing(graph)
+    print('Answer')
+    print("Path:", s, ", Cost:", get_cost(s, graph))
+
 
 if __name__ == "__main__":
     main()

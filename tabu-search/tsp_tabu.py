4c6f8da964994792806934aaf7494fdf660a451c
masaponto <masaponto@gmail.com>
2016-10-29 12:24:39 +0900

    [Refactor] Refactoring and add doctest

--- tabu-search/tsp_tabu.py
+++ tabu-search/tsp_tabu.py
@@ -12,6 +12,22 @@ def print_graph(m, graph):
 
 
 def swap_list_element(lst, i, j):
+    """Swap list element
+
+    Args:
+    lst [int]: one soluton
+    i int
+    j int
+
+    Returns:
+    lst [int]: swaped soluton
+
+    Examples:
+    >>> lst = [1,2,3,4]
+    >>> swap_list_element(lst, 0, 3)
+    [4, 2, 3, 1]
+    """
+
     tmp = lst[i]
     lst[i] = lst[j]
     lst[j] = tmp
@@ -19,12 +35,24 @@ def swap_list_element(lst, i, j):
 
 
 def get_neighbors(s):
+    """Generate neighbors of one solution s
+
+    Args:
+    s [int]: one soluton
+
+    Returns:
+    [[int]]: neigbors
+
+    Examples:
+    >>> lst = [1,2,3]
+    >>> get_neighbors(lst)
+    [[2, 1, 3], [3, 2, 1], [1, 3, 2]]
+    """
     neighbors = []
-    for i in range(len(s)):
-        for j in range(len(s)):
-            if i != j:
-                tmp = s[:]
-                neighbors.append(swap_list_element(tmp, i, j))
+    for i in range(len(s) - 1):
+        for j in range(i + 1, len(s)):
+            tmp = s[:]
+            neighbors.append(swap_list_element(tmp, i, j))
 
     return neighbors
 
@@ -33,12 +61,12 @@ def get_cost(path, graph):
     return sum([graph[path[i]][path[i + 1]] for i in range(len(path) - 1)])
 
 
-def find_better_solusion(lst, s, graph):
-    costs = [get_cost(x, graph) for x in lst]
+def find_better_solusion(feisible_list, s, graph):
+    costs = [get_cost(x, graph) for x in feisible_list]
     i = costs.index(min(costs))
 
     if costs[i] < get_cost(s, graph):
-        return lst[i]
+        return feisible_list[i]
     else:
         return s
 
@@ -76,4 +104,6 @@ def main():
     print(s, ":", get_cost(s, graph))
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
